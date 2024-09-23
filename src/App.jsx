b1752f4df64fae46088edb4176a4451fce2bf6b6
Sean McCarty <soxfansean@gmail.com>
2024-09-23 19:00:50 -0400

    Continue defining state & add warning for invalid inputs

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,35 @@ function randomPoint() {
   }
 }
 
+function validNumber(num, totalPoints) {
+  return (num > 0 && num <= totalPoints)|| num == ""
+}
+
 function App() {
 
   const numPoints = 100
   const [data, setData] = useState([...Array(numPoints)].map(() => randomPoint()))
   const [centers, setCenters] = useState([])
   const [method, setMethod] = useState("Random")
+  const [warning, setWarning] = useState(false)
+  const [k, setK] = useState(2)
 
   const newPoints = () => {
     const points = [...Array(numPoints)].map(() => randomPoint())
 
     setData(() => points)
+  }
+
+  const invalid = prevNum => {
+    setWarning(() => true)
+
+    return prevNum
+  }
+
+  const valid = num => {
+    setWarning(() => false)
+
+    return num
   }
 
 
@@ -31,7 +49,9 @@ function App() {
       <h1>Visualization of KMeans</h1>
 
       <label htmlFor="clusters">Number of Clusters (k):</label>
-      <input type="number" name="clusters" id="clusters" />
+      <input type="number" name="clusters" id="clusters" value={k} onChange={e => setK(prevK => validNumber(e.target.value, numPoints)? valid(e.target.value): invalid(prevK))}/>
+
+      {warning && <div>Please enter a number between 1 and {numPoints}</div>}
 
       <label htmlFor="method">Center Initialization Method:</label>
       <select name="method" id="method" value={method} onChange={e => setMethod(() => e.target.value)}>
@@ -42,7 +62,7 @@ function App() {
       </select>
 
       <button>Step Through KMeans</button>
-      
+
       <button>Run to Convergence</button>
 
       <button onClick={newPoints}>Generate New Dataset</button>
