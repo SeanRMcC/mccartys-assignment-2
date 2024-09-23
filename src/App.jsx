4ec0f7b02e58dea408e0b0d9b3dc3a7ecfd2ee4b
Sean McCarty <soxfansean@gmail.com>
2024-09-23 18:18:04 -0400

    Populate scatter plot of random data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import { useState } from 'react'
+import Plot from 'react-plotly.js'
+
+function randomPoint() {
+  const x = (Math.random() * 20) - 10
+  const y = (Math.random() * 20) - 10
+  return {
+    x,
+    y
+  }
+}
 
 function App() {
+
+  const numPoints = 100
+  const [data, setData] = useState([...Array(numPoints)].map(() => randomPoint()))
+
+  const newPoints = () => {
+    const points = [...Array(numPoints)].map(() => randomPoint())
+
+    setData(() => points)
+  }
+
 
   return (
     <>
       <h1>Hello</h1>
+      <button onClick={newPoints}>New Points</button>
+      <Plot
+        data={[
+          {
+            x: data.map(point => point.x),
+            y: data.map(point => point.y),
+            type: 'scatter',
+            mode: 'markers',
+            marker: {color: 'red'},
+          },
+        ]}
+        layout={ {width: 650, height: 600, title: 'K-means'} }
+      />
     </>
   )
 }
