703e3543cd33acd14db5cb0fcda33c93a8246a8d
Sean McCarty <soxfansean@gmail.com>
2024-09-23 18:46:40 -0400

    Add all necessary html elements & begin to create necessary state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Plot from 'react-plotly.js'
+import "./styles.css"
 
 function randomPoint() {
   const x = (Math.random() * 20) - 10
   const y = (Math.random() * 20) - 10
   return {
     x,
-    y
+    y,
+    color: "blue"
   }
 }
 
@@ -14,6 +16,8 @@ function App() {
 
   const numPoints = 100
   const [data, setData] = useState([...Array(numPoints)].map(() => randomPoint()))
+  const [centers, setCenters] = useState([])
+  const [method, setMethod] = useState("Random")
 
   const newPoints = () => {
     const points = [...Array(numPoints)].map(() => randomPoint())
@@ -23,9 +27,28 @@ function App() {
 
 
   return (
-    <>
-      <h1>Hello</h1>
-      <button onClick={newPoints}>New Points</button>
+    <div className="app-container">
+      <h1>Visualization of KMeans</h1>
+
+      <label htmlFor="clusters">Number of Clusters (k):</label>
+      <input type="number" name="clusters" id="clusters" />
+
+      <label htmlFor="method">Center Initialization Method:</label>
+      <select name="method" id="method" value={method} onChange={e => setMethod(() => e.target.value)}>
+        <option value="Random">Random</option>
+        <option value="Farthest First">Farthest First</option>
+        <option value="KMeans++">KMeans++</option>
+        <option value="Manual">Manual</option>
+      </select>
+
+      <button>Step Through KMeans</button>
+      
+      <button>Run to Convergence</button>
+
+      <button onClick={newPoints}>Generate New Dataset</button>
+
+      <button>Reset Algorithm</button>
+
       <Plot
         data={[
           {
@@ -33,12 +56,12 @@ function App() {
             y: data.map(point => point.y),
             type: 'scatter',
             mode: 'markers',
-            marker: {color: 'red'},
+            marker: {color: data.map(point => point.color)},
           },
         ]}
-        layout={ {width: 650, height: 600, title: 'K-means'} }
+        layout={ {width: 6, height: 560, title: `K-means: ${method}`} }
       />
-    </>
+    </div>
   )
 }
 
